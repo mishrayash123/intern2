3b00e4dca6d5e29eb29373a3c7866db5b4627bff
yash kumar mishra <mishrayash3778@gmail.com>
2023-05-31 17:36:43 +0530

    nk

--- server/index.js
+++ server/index.js
@@ -4,9 +4,20 @@ const express = require('./config/express');
 
 const app = require('express')();
 
-if(process.env.NODE_ENV== "production"){
-    app.use(express.static("client/build"));
+// Your code
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
 }
+// Your code
+
 
 dbConnection().then(() => {
 
